8302f1c9e8d5ef8e62ea71d567afef783b27aa06
raamadithyan <82455071+raamadithyan@users.noreply.github.com>
2023-04-25 11:52:09 +0000

    input done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,9 +1,47 @@
-import {useState,useEffect} from 'react';
+import { useEffect, useState } from 'react';
+import { copy, linkIcon, loader, tick } from './assets';
 
 export default function Demo() {
-return (
- <div>
-Demo
-</div>
-);
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert('sadfgf');
+  }
+
+  return (
+    <section className=" mt-8 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          action="submit"
+          onSubmit={handleSubmit}
+          className="relative flex justify-center items-center"
+        >
+          <img
+            src={linkIcon}
+         
+            alt="link_icon "
+            className="absolute left-0 my-5 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter Url"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 
+peer-focus:text-gray-700  "
+          >
+            <span className="w-fit ">Submit</span>
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
